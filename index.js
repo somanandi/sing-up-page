25f392637b25e62987c26bdd34b7feea56daeeeb
somanandi <soman8698@gmail.com>
2023-12-06 11:43:35 +0530

    first commite

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.html page js
+function myFunction() 
+{
+alert("login sucessfull!");
+}
+function changeText(id)
+{
+ id.innerHTML = "!login!";
+}
+function myFunction1() 
+{
+alert("registation cancle");
+}
